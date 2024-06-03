8d7408c398fc942bc8add354536c5e3909b7831c
YagmurGULEC <yagmurgulec89@gmail.com>
2024-06-03 10:32:55 -0400

    new template

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
-  
 
-    
+    fetch("/js/cv.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error
+                    (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) =>
+            console.log(data))
+        .catch((error) =>
+            console.error("Unable to fetch data:", error));
+
+
 
 
 });
