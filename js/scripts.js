9d21903fa3c17e11d3f6752b1ae8d74029680506
YagmurGULEC <yagmurgulec89@gmail.com>
2024-06-12 14:55:52 -0400

    Next prev buttons finished

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,7 @@ async function fetchdata(url) {
   return data;
 }
 
+
 class Blog{
   static section=getElement('.blogs');
   static blogTitle=getElement('.blog-title');
@@ -30,14 +31,15 @@ class Blog{
     article.className="mb-4"
     this.link=document.createElement('a');
     this.link.className="link-dark";
-    this.link.href="./blog-post.html";
+    this.link.href="#";
     this.link.innerHTML=`<h3>${this.title}</h3>`;
     article.appendChild(this.link);
     if (Blog.section)
       Blog.section.prepend(article) ;
-    
-  }
+    }
 }
+
+//image class to create images on home page
 
 class Image {
   static section=getElement('.photos');
@@ -69,9 +71,7 @@ class Image {
             Image.modal.querySelector('img').src=this.url;
             Image.modal.querySelector('.modal-title').textContent=this.img.title;
             Image.modal.querySelector('.model-description').textContent=this.img.alt;
-         
-        
-  })
+         })
   }
 }
 
@@ -114,63 +114,147 @@ class Movies{
     if (Movies.section)
       Movies.section.appendChild(div);
   }
+}
+
+
+const createArticles=(title,description,content)=>{
+      let blog=new Blog(title,description,content);
+        blog.createArticle();
+        blog.link.onclick=()=>{
+          console.log(title)
+          sessionStorage.setItem('blog',JSON.stringify({title:title,content:content}));
+          blog.link.href="./blog-post.html";
+        }
+}
+const createPhotos=(url,title,description,index)=>{
+  let img=new Image(url,index);
+  img.createDiv(title,description);
+  img.openModal();
+}
+
+let start=0;
+
+let numberPerPage=3;
+let end=start+numberPerPage;
+const nextButton=document.querySelector('.next');
+const prevButton=document.querySelector('.prev');
+const fetchAll=(selectedLanguage)=>{
+  fetchdata('./cv.json').then(data => 
+    Object.keys(data).forEach(key=>{
+      if (key==="blogs")
+        {
+          data[key].slice(start,end).forEach((element,index)=>{
+            if (element[selectedLanguage])  
+              createArticles(element[selectedLanguage].title,element[selectedLanguage].description,element[selectedLanguage].content)
+        
+          }) 
+          nextButton.onclick=(e)=>{
+            prevButton.classList.remove('disabled');
+            start=end;
+            end=start+numberPerPage;
+            Blog.section.innerHTML="";
+            data[key].slice(start,end).forEach((element,index)=>{
+              if (element[selectedLanguage])  
+                createArticles(element[selectedLanguage].title,element[selectedLanguage].description,element[selectedLanguage].content)
+             
+            })
+            if (end>=data[key].length) nextButton.classList.add('disabled');
+          
+            }
+             
+           
+            prevButton.onclick=(e)=>{
+              nextButton.classList.remove('disabled')
+              start=start-numberPerPage;
+              end=start+numberPerPage;
+             Blog.section.innerHTML="";
+                  data[key].slice(start,end).forEach((element,index)=>{
+                if (element[selectedLanguage])  
+                  createArticles(element[selectedLanguage].title,element[selectedLanguage].description,element[selectedLanguage].content)
+              })
+              if (start<=0) prevButton.classList.add('disabled');
+          
+            }
+             
+        
+          }
+        if (key==="photos")
+        {
+          data[key].forEach((element,index) => {
+            //if (element[selectedLanguage])
+              createPhotos(element.url,element[selectedLanguage].title,element[selectedLanguage].description);
+          })
+        }
+       if (key==="movies")
+        {
+          data[key].forEach((element,index)=>{
+            if (element[selectedLanguage]){
+              let movie=new Movies(element.title,element.description,element.photo);
+              movie.createMovie();
+              movie.link.onclick=()=>{
+               sessionStorage.setItem('movie',JSON.stringify(element));
+              }
+            }
+           
+          })
+
+        }
+      }
+    )
+  );
+}
+
+const deleteAll=()=>{
+  Blog.section.innerHTML="";
+  Image.section.innerHTML="";
+  Movies.section.innerHTML="";
 
 }
 
-const selectedLanguage = "English"  
 
-fetchdata('./cv.json').then(data => 
+document.addEventListener('DOMContentLoaded',()=>{
+  let selectedLanguage=document.querySelector('.selected').innerHTML;
 
-      data.photos.forEach((element,index) => {
-      let img=new Image(element.url,index);
-      img.createDiv(element[selectedLanguage].title,element[selectedLanguage].description);
-      img.openModal();
-    })
-  );
+  fetchAll(selectedLanguage);
 
-fetchdata('./cv.json').then(data =>
-  data.blogs.forEach((element,index)=>{
-    let blog=new Blog(element[selectedLanguage].title,element[selectedLanguage].description,element[selectedLanguage].content);
-    blog.createArticle();
-    blog.link.onclick=()=>{
-      localStorage.setItem('blog',JSON.stringify(element));
-    }
-  })
-);
-
-fetchdata('./cv.json').then(data => 
-  data.movies.forEach((element,index)=>{
-    let movie=new Movies(element.title,element.description,element.photo);
-    movie.createMovie();
-    movie.link.onclick=()=>{
-      localStorage.setItem('movie',JSON.stringify(element));
-    }
-  })
-);
-
-
-window.addEventListener('DOMContentLoaded', event => {
   window.onload=()=>{
-    if (Blog.blogTitle)
-      Blog.blogTitle.textContent=JSON.parse(localStorage.getItem('blog'))[selectedLanguage].title;
-    if (Blog.blogContent)
-      JSON.parse(localStorage.getItem('blog'))[selectedLanguage].content.forEach(element => {
-        let p=document.createElement('p');
-        p.className="fs-5 mb-4";
-        p.textContent=element;
-        Blog.blogContent.appendChild(p);
-      });
-    if (Movies.movieTitle)
-      Movies.movieTitle.textContent=JSON.parse(localStorage.getItem('movie')).title;
-    if (Movies.moviePhoto)
-      Movies.moviePhoto.src=JSON.parse(localStorage.getItem('movie')).photo;
-    if (Movies.movieContent)
-      JSON.parse(localStorage.getItem('movie'))[selectedLanguage].content.forEach(element => {
-        let p=document.createElement('p');
-        p.className="fs-5 mb-4";
-        p.textContent=element;
-        Movies.movieContent.appendChild(p);
-      });
+    if ((Blog.blogTitle) && (Blog.blogContent))
+      {
+        Blog.blogTitle.textContent=JSON.parse(sessionStorage.getItem('blog')).title;
+        JSON.parse(sessionStorage.getItem('blog')).content.forEach(element => {
+                  let p=document.createElement('p');
+                  p.className="fs-5 mb-4";
+                  p.textContent=element;
+                  Blog.blogContent.appendChild(p);
+                });
+      }
+
+      if ((Movies.movieTitle) && (Movies.movieContent) && (Movies.moviePhoto))
+        {
+          Movies.movieTitle.textContent=JSON.parse(localStorage.getItem('movie')).title;
+          Movies.moviePhoto.src=JSON.parse(localStorage.getItem('movie')).photo;
+          
+          JSON.parse(sessionStorage.getItem('movie'))[selectedLanguage].content.forEach(element => {
+                    let p=document.createElement('p');
+                    p.className="fs-5 mb-4";
+                    p.textContent=element;
+                    Movies.movieContent.appendChild(p);
+                  });
+        }
   }
   
-});
+  document.querySelectorAll('.dropdown-item').forEach(item=>{
+  
+    
+    item.onclick=(e)=>{
+     let temp=selectedLanguage;
+      selectedLanguage=e.target.innerHTML;
+      document.querySelector('.selected').innerHTML=selectedLanguage;
+      e.target.innerHTML=temp;
+      deleteAll();
+      fetchAll(selectedLanguage,start,stop);
+    }
+  })
+
+})
+
