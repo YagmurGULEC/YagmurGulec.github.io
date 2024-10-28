e8ac739b9f6ccd048f6a54901fe086a14cd5654a
Yagmur GULEC <yagmurgulec89@gmail.com>
2024-10-28 15:21:56 -0400

    Just a small correction

--- src/components/Professional.js
+++ src/components/Professional.js
@@ -11,7 +11,7 @@ export default function Professional({state}){
                     <h3 className="mb-0">Beyond the Cloud Backend Developer Level 2</h3>
                     <div className="subheading mb-3">{state==="English"?"Information and Communications Technology Council(ICTC)" :"Le Conseil des technologies de l'information et des communications (CTIC)"}</div>
                     <p> {state==="English"?"Selected for 10-week customized training program, funded by Upskill Canada (powered by Palette Skills) and the Government of Canada." :"Sélectionné pour un programme de formation personnalisé de 10 semaines, financé par Upskill Canada (powered by Palette Skills) et le gouvernement du Canada."} </p>
-                    <p> {"Courses included: Git version control, Django Full-stack, Amazon Elastic Kubernetes Service, Microsoft Azure Fundamentals"}</p>
+                    <p> {state==="English"?"Courses included:":"Cours inclus: "}Git version control, Django Full-stack, Amazon Elastic Kubernetes Service, Microsoft Azure Fundamentals</p>
                     
                 </div>
                 <div className="flex-shrink-0"><span className="text-primary">{state==="English"? "July 2024 - Present" :"Juillet 2024 - Actuellement"}</span></div>
